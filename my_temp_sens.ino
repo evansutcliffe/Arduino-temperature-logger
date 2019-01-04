8b8374db7c0e7f6b13937e89f8cf70888c4e3ff0
evansutcliffe <30503110+evansutcliffe@users.noreply.github.com>
2019-01-04 16:02:35 +0000

    Add files via upload

---
+++ my_temp_sens.ino
@@ -0,0 +1,23 @@
+#include <dht.h>
+#include <Time.h>
+dht DHT;
+
+#define DHT11_PIN 7
+double chk;
+void digitalClockDisplay();
+
+void setup(){
+  Serial.setTimeout(50)
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(1500);
+}
+
